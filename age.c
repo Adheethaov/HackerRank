3b96a02d411556e9a5ced3ff67b6fd5edc9af264
Adheetha@crazycodez <33346761+Adheethaov@users.noreply.github.com>
2020-03-03 21:59:08 +0530

    Create age.c

---
+++ age.c
@@ -0,0 +1,26 @@
+/*
+Sample Input
+
+17
+
+
+
+Sample Output
+
+You are 17 years old!
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int a;
+    scanf("%d",&a);
+    printf("You are %d years old!",a);
+    return 0;
+}
